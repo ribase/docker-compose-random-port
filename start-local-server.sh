a135bd86ad56f363f9af169e139fbdefa57335d8
Sebastian Thadewald <you@example.com>
2018-04-30 13:49:58 +0200

    init

---
+++ start-local-server.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+COUNTERHTTP=80
+COUNTERMYSQL=3360
+
+function determinePortHttp {
+    while [ 1 ]
+    do
+        port=`lsof -i :${COUNTERHTTP}`
+        echo "Trying ${COUNTERHTTP}"
+        if [[ "$port" = *"com.docke"* ]]
+        then
+                COUNTERHTTP=$((COUNTERHTTP + 1))
+        else
+                execDocker
+cat <<EOF
+*************************************
+** MYSQL PORT: ${COUNTERMYSQL}     **
+** HTTP PORT: ${COUNTERHTTP}       **
+*************************************
+EOF
+                exit
+        fi
+    done
+}
+
+function determinePortMysql {
+    while [ 1 ]
+    do
+        port=`lsof -i :${COUNTERMYSQL}`
+        echo "Trying ${COUNTERMYSQL}"
+        if [[ "$port" = *"com.docke"* ]]
+        then
+                COUNTERMYSQL=$((COUNTERMYSQL + 1))
+        else
+                determinePortHttp
+        fi
+    done
+}
+
+
+function execDocker {
+cat <<EOF
+    ██████╗  ██████╗  ██████╗██╗  ██╗███████╗██████╗ ███╗   ███╗ █████╗  ██████╗ ██╗ ██████╗
+    ██╔══██╗██╔═══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗████╗ ████║██╔══██╗██╔════╝ ██║██╔════╝
+    ██║  ██║██║   ██║██║     █████╔╝ █████╗  ██████╔╝██╔████╔██║███████║██║  ███╗██║██║
+    ██║  ██║██║   ██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗██║╚██╔╝██║██╔══██║██║   ██║██║██║
+    ██████╔╝╚██████╔╝╚██████╗██║  ██╗███████╗██║  ██║██║ ╚═╝ ██║██║  ██║╚██████╔╝██║╚██████╗
+    ╚═════╝  ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝ ╚═════╝
+EOF
+
+    PORT=${COUNTERHTTP} MYSQL=${COUNTERMYSQL} docker-compose up --build --remove-orphans  -d
+
+    echo "Add host to /etc/hosts"
+
+    if grep -q "www.docker.local" /etc/hosts; then
+        echo "Exists"
+    else
+        sudo bash -c "echo '127.0.0.1     www.docker.local' >> /etc/hosts"
+    fi
+}
+
+determinePortMysql
